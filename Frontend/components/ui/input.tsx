925999974ac1d50b444cdda89db884a8b1f88509
Mujtabashah4 <mujtabashah1204@gmail.com>
2025-12-18 17:55:27 +0500

    Fix UI imports for lib/utils to work on Vercel

--- Frontend/components/ui/input.tsx
+++ Frontend/components/ui/input.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 
-import { cn } from '@/lib/utils'
+// Use relative import instead of alias to avoid issues on Vercel
+import { cn } from '../../lib/utils'
 
 const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<'input'>>(
   ({ className, type, ...props }, ref) => {
